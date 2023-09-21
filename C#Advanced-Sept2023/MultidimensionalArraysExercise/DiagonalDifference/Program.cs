34cc94d9651a9a9a368d72985402058f960fe68b
StefanDokov <stefandokov2211@gmail.com>
2023-09-21 21:44:58 +0300

    adding tasks
    adding more tasks

---
+++ C#Advanced-Sept2023/MultidimensionalArraysExercise/DiagonalDifference/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+int n  = int.Parse(Console.ReadLine()); 
+
+int[,] matrix = new int[n, n];
+
+int mainDiagonal = 0;
+int oppositeDiagonal = 0;
+
+int h = n - 1;
+int g = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Queue<int> q = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = q.Dequeue();
+    }
+}
+
+for (int i = 0;i < n; i++)
+{
+    for (int j = 0;j < n; j++)
+    {
+        if (i == j)
+        {
+            mainDiagonal += matrix[i,j];
+        }
+
+       
+    }
+}
+
+while (h >= 0)
+{
+    oppositeDiagonal += matrix[g, h];
+
+    h--;
+    g++;
+}
+
+Console.WriteLine(Math.Abs(mainDiagonal - oppositeDiagonal));
---
+++ C#Advanced-Sept2023/MultidimensionalArraysExercise/SquaresinMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+int[] n = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int[,] matrix = new int[n[0], n[1]];
+
+int eqalCells = 0;
+
+for (int i = 0; i < n[0]; i++)
+{
+    Queue<char> q = new Queue<char>(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(char.Parse));
+
+    for (int j = 0; j < n[1]; j++)
+    {
+        matrix[i, j] = q.Dequeue();
+    }
+}
+
+for (int i = 0; i < n[0] - 1; i++)
+{
+    for (int j = 0;j < n[1] - 1; j++)
+    {
+
+        if (matrix[i, j] == matrix[i + 1, j] && matrix[i,j] == matrix[i, j+ 1] && matrix[i + 1, j + 1] == matrix[i, j])
+        {
+            eqalCells++;
+        }
+
+
+
+    }
+}
+
+
+Console.WriteLine(eqalCells);
