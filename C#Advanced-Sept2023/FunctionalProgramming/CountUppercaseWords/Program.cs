94de1877db155b89ccc65f0659793a0cb680f05d
StefanDokov <stefandokov2211@gmail.com>
2023-09-30 17:50:02 +0300

    funcs
    funcs and actions

---
+++ C#Advanced-Sept2023/FunctionalProgramming/CountUppercaseWords/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+
+string[] uppers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(s => char.IsUpper(s[0]))
+    .ToArray();
+    
+
+foreach (string word in uppers)
+{
+    Console.WriteLine(word);
+}
---
+++ C#Advanced-Sept2023/FunctionalProgramming/SortEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+using System.Runtime.InteropServices;
+
+int[] numbers = Console.ReadLine()
+    .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .ToArray();
+
+
+string result = String.Join(", ", numbers);
+Console.WriteLine(result);
