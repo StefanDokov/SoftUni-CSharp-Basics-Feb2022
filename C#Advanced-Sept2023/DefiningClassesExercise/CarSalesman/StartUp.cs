a60d41be806cbb14c38cd6baf21e12247b134eaa
StefanDokov <stefandokov2211@gmail.com>
2023-10-05 17:51:51 +0300

    more
    tasks

---
+++ C#Advanced-Sept2023/DefiningClassesExercise/CarSalesman/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSalesman
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new();
+            List<Engine> engines = new();
+
+            int countEngines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countEngines; i++)
+            {
+                string[] engineProperties = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Engine engine = CreateEngine(engineProperties);
+
+                engines.Add(engine);
+            }
+
+            int countCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countCars; i++)
+            {
+                string[] carProperties = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = CreateCar(carProperties, engines);
+
+                cars.Add(car);
+            }
+        }
+    }
