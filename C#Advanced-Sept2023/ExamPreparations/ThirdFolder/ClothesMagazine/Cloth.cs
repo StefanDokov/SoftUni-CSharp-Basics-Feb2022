e70c2aa1fddf2786e19f5b85f144f4a0f3c02d76
StefanDokov <stefandokov2211@gmail.com>
2023-10-21 08:48:39 +0300

    more
    tasks

--- C#Advanced-Sept2023/ExamPreparations/ThirdFolder/ClothesMagazine/Cloth.cs
+++ C#Advanced-Sept2023/ExamPreparations/ThirdFolder/ClothesMagazine/Cloth.cs
@@ -2,5 +2,41 @@
 {
     public class Cloth
     {
+        public Cloth(string color, int size, string type)
+        {
+            Color = color;
+			Size = size;
+			Type = type;
+        }
+
+        private string color;
+
+		public string Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+
+		private int size;
+
+		public int Size
+		{
+			get { return size; }
+			set { size = value; }
+		}
+
+		private string type;
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+        public override string ToString()
+        {
+            return $"Product: {Type} with size {Size}, color {Color}";
+        }
+
     }
 }
--- C#Advanced-Sept2023/ExamPreparations/ThirdFolder/ClothesMagazine/Magazine.cs
+++ C#Advanced-Sept2023/ExamPreparations/ThirdFolder/ClothesMagazine/Magazine.cs
@@ -1,6 +1,82 @@
-﻿namespace ClothesMagazine
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClothesMagazine
 {
     public class Magazine
     {
-    }
+        public Magazine(string type, int capacity)
+        {
+			Type = type;
+            Capacity = capacity;
+			Clothes = new List<Cloth>();
+        }
+
+        private string type;
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+		private int capacity;
+
+		public int Capacity
+		{
+			get { return capacity; }
+			set { capacity = value; }
+		}
+
+		private List<Cloth> clothes;
+
+		public List<Cloth> Clothes
+		{
+			get { return clothes; }
+			set { clothes = value; }
+		}
+
+		public void AddCloth(Cloth cloth)
+		{
+			if (Clothes.Count < Capacity)
+			{
+				Clothes.Add(cloth);
+			}
+		}
+
+		public bool RemoveCloth(string color)
+		{
+			return Clothes.Remove(Clothes.Where(c => c.Color == color).FirstOrDefault());
+		}
+
+		public Cloth GetSmallestCloth()
+		{
+			return Clothes.OrderBy(c => c.Size).FirstOrDefault();
+		}
+
+		public Cloth GetCloth(string color)
+		{
+			return Clothes.Where(c => c.Color == color).FirstOrDefault();
+		}
+
+		public int GetClothCount()
+		{
+			return Clothes.Count;
+		}
+
+		public string Report()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendLine($"{Type} magazine contains:");
+
+			foreach (var clot in Clothes.OrderBy(c => c.Size))
+			{
+				sb.AppendLine(clot.ToString());
+			}
+
+			return sb.ToString().TrimEnd();
+		}
+
+	}
 }
