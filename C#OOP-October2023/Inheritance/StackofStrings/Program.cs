fbd48e5e5970d4e41a7647dfcf72de12f55d9484
StefanDokov <stefandokov2211@gmail.com>
2023-10-26 17:13:26 +0300

    csharp
    oop module

---
+++ C#OOP-October2023/Inheritance/StackofStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+namespace CustomStack
+{
+
+    public class StartUp
+    {
+
+        static void Main() 
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(new List<string>()
+            {
+                "123",
+                "456",
+                "789",
+                "Gogi"
+            });
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
