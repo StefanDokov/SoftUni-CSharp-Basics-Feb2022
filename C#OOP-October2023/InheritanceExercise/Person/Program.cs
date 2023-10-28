4709ff3a3cd768766b67abd1166a2abfd6fc3ba9
StefanDokov <stefandokov2211@gmail.com>
2023-10-28 19:20:22 +0300

    add
    task solutions

---
+++ C#OOP-October2023/InheritanceExercise/Person/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+
+namespace Person;
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+    public virtual int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                age = value;
+            }
+        }
+    }
+
+        public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+
+}
+
+
+public class Child : Person
+{
+    public Child(string name, int age)
+        :base(name, age)
+    { 
+    }
+
+    public override int Age { get => base.Age; set{
+            if (value <= 15) 
+            {
+                base.Age = value;
+            }
+           }
+    }
+
+
+
+}
