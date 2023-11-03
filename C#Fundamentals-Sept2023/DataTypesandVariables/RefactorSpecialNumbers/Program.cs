427103390a5116184e5e32c15d06979d100cad49
StefanDokov <stefandokov2211@gmail.com>
2023-11-03 15:46:11 +0200

    some
    extra tasks added

---
+++ C#Fundamentals-Sept2023/DataTypesandVariables/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿int kolko = int.Parse(Console.ReadLine());
+int obshto = 0;
+int takova = 0;
+bool toe = false;
+for (int ch = 1; ch <= kolko; ch++)
+{
+    takova = ch;
+    while (ch > 0)
+    {
+        obshto += ch % 10;
+        ch = ch / 10;
+    }
+
+    if(obshto == 5 || obshto == 7 || obshto == 11){
+       toe = true;
+    
+     }
+    else
+    {
+        toe = false;
+    }
+    Console.WriteLine("{0} -> {1}", takova, toe);
+    obshto = 0;
+    ch = takova;
+
+}
--- C#Fundamentals-Sept2023/DataTypesandVariables/RefactorVolumeofPyramid/Program.cs
+++ C#Fundamentals-Sept2023/DataTypesandVariables/RefactorVolumeofPyramid/Program.cs
@@ -1,12 +1,12 @@
 ﻿
 
 
-double dul, sh, V = 0;
+double dul, sh, h, V = 0;
 Console.Write("Length: ");
 dul = double.Parse(Console.ReadLine());
 Console.Write("Width: ");
 sh = double.Parse(Console.ReadLine());
 Console.Write("Height: ");
-V = double.Parse(Console.ReadLine());
-V = (dul + sh + V);
+h = double.Parse(Console.ReadLine());
+V = ((dul * sh) * h) / 3;
 Console.WriteLine($"Pyramid Volume: {V:f2}");
