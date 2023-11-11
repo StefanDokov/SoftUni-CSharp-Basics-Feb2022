14f3f9ac777807dccc7c3764aeccb56e0f9333d0
StefanDokov <stefandokov2211@gmail.com>
2023-11-11 18:18:54 +0200

    add
    more tasks

---
+++ C#Fundamentals-Sept2023/DataTypesandVariablesExercise/BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+int n = int.Parse(Console.ReadLine());
+
+double v = double.MinValue;
+string bestKeg = null;
+
+for (int i = 0; i < n; i++)
+{
+    string model = Console.ReadLine();
+
+    double r = double.Parse(Console.ReadLine());
+
+    int h = int.Parse(Console.ReadLine());
+
+    double mid = Math.PI * (r * r) * h;
+
+    if (mid > v)
+    {
+        v = mid;
+        bestKeg = model;
+    }
+
+
+}
+
+Console.WriteLine(bestKeg);
---
+++ C#Fundamentals-Sept2023/DataTypesandVariablesExercise/WaterOverflow/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+int capacity = 255;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int current = int.Parse(Console.ReadLine());
+
+    if (capacity - current >= 0)
+    {
+        capacity -= current;
+    }
+    else
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+
+}
+
+Console.WriteLine(255 - capacity);
