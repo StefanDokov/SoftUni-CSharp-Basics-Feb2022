d9c9d309b0a03bd4d8518d46b32fe77fc58cfafc
StefanDokov <stefandokov2211@gmail.com>
2023-11-26 15:55:44 +0200

    add
    more tasks

--- C#Fundamentals-Sept2023/ArraysExercise/PascalTriangle/Program.cs
+++ C#Fundamentals-Sept2023/ArraysExercise/PascalTriangle/Program.cs
@@ -1,2 +1,37 @@
 ﻿
+using System;
 
+class PascalTriangle
+{
+    static void Main()
+    {
+        int numRows = int.Parse(Console.ReadLine()); 
+
+        GeneratePascalTriangle(numRows);
+    }
+
+    static void GeneratePascalTriangle(int numRows)
+    {
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculateBinomialCoefficient(i, j) + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculateBinomialCoefficient(int n, int k)
+    {
+        if (k == 0 || k == n)
+        {
+            return 1;
+        }
+        else
+        {
+            return CalculateBinomialCoefficient(n - 1, k - 1) + CalculateBinomialCoefficient(n - 1, k);
+        }
+    }
+}
---
+++ C#Fundamentals-Sept2023/ArraysExercise/RecursiveFibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 1 || n > 50)
+        {
+            Console.WriteLine("Invalid input. N should be between 1 and 50.");
+        }
+        else
+        {
+            int result = GetFibonacci(n);
+            Console.WriteLine(result);
+        }
+    }
+
+    static int GetFibonacci(int n)
+    {
+        if (n <= 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
