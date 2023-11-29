0a3bef2c437cc06e2cf272452cf604b0657b068c
StefanDokov <stefandokov2211@gmail.com>
2023-11-29 17:25:24 +0200

    add some
    tasks

---
+++ C#Fundamentals-Sept2023/Methods/Mathoperations/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+int first = int.Parse(Console.ReadLine());
+
+string operand = Console.ReadLine();
+
+int second = int.Parse(Console.ReadLine()); 
+
+Console.WriteLine(Calc(first, operand, second));
+
+
+static double Calc(int a, string @operator, int b)
+{
+
+    double result = 0;
+
+    switch (@operator)
+    {
+        case "*":
+            result = a * b;
+            break;
+
+    case "/":
+            result = a / b;
+            break;
+            case "+":
+            result = a + b;
+            break;
+            case "-":
+            result = a - b;
+            break;
+
+
+
+
+    }
+    return result;
+
+}
--- C#Fundamentals-Sept2023/Methods/MultiplyEvensbyOdds/Program.cs
+++ C#Fundamentals-Sept2023/Methods/MultiplyEvensbyOdds/Program.cs
@@ -1,3 +1,48 @@
 ﻿
 
 
+using System.Security.Cryptography;
+
+int n = Math.Abs(int.Parse(Console.ReadLine()));
+
+
+int result = GetSumOfEvenDigits(n) * GetSumOfOddDigits(n);
+
+Console.WriteLine(result);
+
+
+static int GetSumOfEvenDigits(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int mid = n % 10;
+
+        if (mid % 2 == 0)
+        {
+            sum += mid;
+        }
+       
+
+        n /= 10;
+    }
+    return sum;
+}
+
+static int GetSumOfOddDigits(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int mid = n % 10;
+
+        if (mid % 2 != 0)
+        {
+            sum += mid;
+        }
+
+
+        n /= 10;
+    }
+    return sum;
+}
