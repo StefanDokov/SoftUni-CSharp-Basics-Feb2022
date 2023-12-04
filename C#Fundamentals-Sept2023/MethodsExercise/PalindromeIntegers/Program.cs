def80bcbbd4a9c7b8d35e76d4a1eda6a08367666
StefanDokov <stefandokov2211@gmail.com>
2023-12-04 17:38:10 +0200

    add
    more tasks

---
+++ C#Fundamentals-Sept2023/MethodsExercise/PalindromeIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input.ToUpper() == "END")
+    {
+        break;
+    }
+
+    if (IsPalindrome(input))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+}
+
+static bool IsPalindrome(string input)
+{
+    string cleanInput = new string(input.Where(char.IsDigit).ToArray());
+
+    return cleanInput == new string(cleanInput.Reverse().ToArray());
+}
