4cca06374258500474550481b0eba49ba61ee3d1
StefanDokov <stefandokov2211@gmail.com>
2023-12-05 20:47:37 +0200

    add
    more

---
+++ C#Fundamentals-Sept2023/MethodsExercise/TopNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= n; i++)
+{
+    if (IsTopNumber(i))
+    {
+        Console.WriteLine(i);
+    }
+}
+
+    static bool IsTopNumber(int number)
+{
+    int sumOfDigits = 0;
+    int num = number;
+
+    while (num > 0)
+    {
+        sumOfDigits += num % 10;
+        num /= 10;
+    }
+
+    if (sumOfDigits % 8 != 0)
+    {
+        return false;
+    }
+
+    num = number;
+
+    while (num > 0)
+    {
+        int digit = num % 10;
+        if (digit % 2 != 0)
+        {
+            return true;
+        }
+
+        num /= 10;
+    }
+
+    return false;
+}
