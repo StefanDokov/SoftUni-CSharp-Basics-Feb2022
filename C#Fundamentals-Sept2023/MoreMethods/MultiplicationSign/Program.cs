56e71dfaab1801bdff429f27a96c014cb10fea4a
StefanDokov <stefandokov2211@gmail.com>
2023-12-20 17:49:22 +0200

    add more tasks
    more task

---
+++ C#Fundamentals-Sept2023/MoreMethods/MultiplicationSign/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+PrintResult(num1, num2, num3);
+
+static void PrintResult(int num1, int num2, int num3)
+{
+    if (IsZero(num1, num2, num3))
+    {
+        Console.WriteLine("zero");
+    }
+    else if (IsNegative(num1, num2, num3))
+    {
+        Console.WriteLine("negative");
+    }
+    else
+    {
+        Console.WriteLine("positive");
+    }
+}
+
+
+static bool IsZero(int num1, int num2, int num3)
+{
+    if (num1 == 0 ||
+        num2 == 0 ||
+        num3 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+static bool IsNegative(int num1, int num2, int num3)
+{
+    int[] numbers = new int[] { num1, num2, num3 };
+    int counter = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (numbers[i] < 0)
+        {
+            counter++;
+        }
+    }
+    if (counter % 2 == 0)
+    {
+        return false;
+    }
+    return true;
+}
