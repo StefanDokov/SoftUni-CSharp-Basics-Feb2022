177764fcb22fdab7ece39e3461e9873612993c31
StefanDokov <stefandokov2211@gmail.com>
2023-12-22 18:40:46 +0200

    add more solutions
    add more stuff

---
+++ C#Fundamentals-Sept2023/Lists/ListofProducts/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+int n = int.Parse(Console.ReadLine());
+
+List<string> list = new List<string>();
+
+for  (int i = 0; i < n; i++)
+{
+
+    string input = Console.ReadLine();
+
+    list.Add(input);
+
+
+}
+int k = 1;
+
+foreach (string input in list.OrderBy(p => p))
+{
+    
+
+    Console.WriteLine($"{k}.{input}");
+
+    k++;
+}
---
+++ C#Fundamentals-Sept2023/Lists/RemoveNegativesandReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+List<int> list = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+
+list.RemoveAll(n  => n < 0);
+
+list.Reverse();
+
+if (list.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+
+
+    Console.WriteLine(string.Join(" ", list));
+}
