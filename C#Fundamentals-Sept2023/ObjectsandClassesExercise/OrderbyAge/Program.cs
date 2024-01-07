3e24603f71109054bda73d252a95bf4309e92155
StefanDokov <stefandokov2211@gmail.com>
2024-01-07 08:18:45 +0200

    add more files
    more files

---
+++ C#Fundamentals-Sept2023/ObjectsandClassesExercise/OrderbyAge/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+Dictionary<string, Person> people = new Dictionary<string, Person>();
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "End")
+    {
+        break;
+    }
+
+    string[] data = input.Split();
+    string name = data[0];
+    string id = data[1];
+    int age = int.Parse(data[2]);
+
+    if (!people.ContainsKey(id))
+    {
+        people[id] = new Person { Name = name, Age = age, ID = id};
+    }
+    else
+    {
+        people[id].Name = name;
+        people[id].Age = age;
+    }
+}
+
+foreach (var person in people.Values.OrderBy(p => p.Age))
+{
+    Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+}
+    
+
+    class Person
+{
+    public string Name { get; set; }
+    public string ID { get; set; } 
+    public int Age { get; set; }
+}
