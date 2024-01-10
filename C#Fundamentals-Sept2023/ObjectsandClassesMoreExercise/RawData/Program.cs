d6d3e4fcf81110bb4e1218a29279a070882bba72
StefanDokov <stefandokov2211@gmail.com>
2024-01-10 17:59:50 +0200

    add more files
    more files

---
+++ C#Fundamentals-Sept2023/ObjectsandClassesMoreExercise/RawData/Program.cs
@@ -0,0 +1,80 @@
+﻿
+var cars = new List<Car>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] cmd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    string model = cmd[0];
+    int engineSpeed = int.Parse(cmd[1]);
+    int enginePower = int.Parse(cmd[2]);
+    int cargoWeight = int.Parse(cmd[3]);
+    string cargoType = cmd[4];
+
+    var engine = new Engine()
+    {
+        EngineSpeed = engineSpeed,
+        EnginePower = enginePower
+    };
+    var cargo = new Cargo()
+    {
+        CargoWeight = cargoWeight,
+        CargoType = cargoType
+    };
+
+    var car = new Car(model, engine, cargo);
+
+    cars.Add(car);
+}
+
+string crgType = Console.ReadLine();
+
+if (crgType == "fragile")
+{
+    foreach (var car in cars.Where(x => x.Cargo.CargoType == "fragile" &&
+                                        x.Cargo.CargoWeight < 1000))
+    {
+        Console.WriteLine($"{car.Model}");
+    }
+
+}
+else if (crgType == "flamable")
+{
+    foreach (var car in cars.Where(x => x.Cargo.CargoType == "flamable" &&
+                                        x.Engine.EnginePower > 250))
+    {
+        Console.WriteLine($"{car.Model}");
+    }
+}
+
+public class Car
+{
+    public Car(string Mdl, Engine engine, Cargo cargo)
+    {
+        Model = Mdl;
+        Engine = engine;
+        Cargo = cargo;
+    }
+
+    public string Model { get; set; }
+
+    public Engine Engine { get; set; }
+
+    public Cargo Cargo { get; set; }
+}
+
+public class Engine
+{
+    public int EngineSpeed { get; set; }
+
+    public int EnginePower { get; set; }
+}
+
+public class Cargo
+{
+    public int CargoWeight { get; set; }
+
+    public string CargoType { get; set; }
+}
---
+++ C#Fundamentals-Sept2023/ObjectsandClassesMoreExercise/ShoppingSpree/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+
+
+Dictionary<string, Person> people = new Dictionary<string, Person>();
+Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+string[] inputPeople = Console.ReadLine().Split(';');
+foreach (var personData in inputPeople)
+{
+    string[] personInfo = personData.Split('=');
+    string personName = personInfo[0];
+    decimal personMoney = decimal.Parse(personInfo[1]);
+    people.Add(personName, new Person(personName, personMoney));
+}
+
+string[] inputProducts = Console.ReadLine().Split(';');
+foreach (var productData in inputProducts)
+{
+    string[] productInfo = productData.Split('=');
+    string productName = productInfo[0];
+    decimal productCost = decimal.Parse(productInfo[1]);
+    products.Add(productName, new Product(productName, productCost));
+}
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] purchaseInfo = command.Split();
+    string personName = purchaseInfo[0];
+    string productName = purchaseInfo[1];
+
+    if (people.ContainsKey(personName) && products.ContainsKey(productName))
+    {
+        people[personName].BuyProduct(productName, products[productName].Cost);
+    }
+}
+
+// Output results
+foreach (var person in people.Values)
+{
+    if (person.Bag.Count > 0)
+    {
+        Console.WriteLine($"{person.Name} - {string.Join(", ", person.Bag)}");
+    }
+    else
+    {
+        Console.WriteLine($"{person.Name} - Nothing bought");
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public decimal Money { get; set; }
+    public List<string> Bag { get; set; }
+
+    public Person(string name, decimal money)
+    {
+        Name = name;
+        Money = money;
+        Bag = new List<string>();
+    }
+
+    public void BuyProduct(string productName, decimal productCost)
+    {
+        if (Money >= productCost)
+        {
+            Money -= productCost;
+            Bag.Add(productName);
+            Console.WriteLine($"{Name} bought {productName}");
+        }
+        else
+        {
+            Console.WriteLine($"{Name} can't afford {productName}");
+        }
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+
+    public Product(string name, decimal cost)
+    {
+        Name = name;
+        Cost = cost;
+    }
+}
