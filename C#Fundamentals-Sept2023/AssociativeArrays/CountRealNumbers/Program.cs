6a3f9e6418f77273a1f294072d855cd94ac5364c
StefanDokov <stefandokov2211@gmail.com>
2024-01-11 17:37:54 +0200

    add more files
    more files

---
+++ C#Fundamentals-Sept2023/AssociativeArrays/CountRealNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+string input = Console.ReadLine();
+
+int[] numbers = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Dictionary<int, int> occurrences = new Dictionary<int, int>();
+
+foreach (int num in numbers)
+{
+    if (occurrences.ContainsKey(num))
+    {
+        occurrences[num]++;
+    }
+    else
+    {
+        occurrences[num] = 1;
+    }
+}
+
+var sortedOccurrences = occurrences.OrderBy(pair => pair.Key);
+
+foreach (var pair in sortedOccurrences)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ C#Fundamentals-Sept2023/AssociativeArrays/OddOccurrences/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+string input = Console.ReadLine();
+
+string[] words = input.Split(' ');
+
+Dictionary<string, int> occurrences = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+foreach (string word in words)
+{
+    if (occurrences.ContainsKey(word))
+    {
+        occurrences[word]++;
+    }
+    else
+    {
+        occurrences[word] = 1;
+    }
+}
+
+var oddOccurrences = occurrences.Where(pair => pair.Value % 2 != 0);
+
+foreach (var pair in oddOccurrences)
+{
+    Console.Write($"{pair.Key.ToLower()} ");
+}
