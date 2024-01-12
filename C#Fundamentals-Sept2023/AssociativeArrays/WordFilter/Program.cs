68387e335ef61ddba652044c2cb3f85aab3ad5d5
StefanDokov <stefandokov2211@gmail.com>
2024-01-12 16:58:49 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/AssociativeArrays/WordFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+string[] words = Console.ReadLine().Split(' ');
+
+FilterAndPrintEvenLengthWords(words);
+
+    static void FilterAndPrintEvenLengthWords(string[] words)
+{
+
+    foreach (string word in words)
+    {
+        if (IsEvenLength(word))
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
+static bool IsEvenLength(string word)
+{
+    return word.Length % 2 == 0;
+}
---
+++ C#Fundamentals-Sept2023/AssociativeArrays/WordSynonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+int inputs = int.Parse(Console.ReadLine());
+var dict = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < inputs; i++)
+{
+    string key = Console.ReadLine(); 
+    string value = Console.ReadLine(); 
+
+    if (!dict.ContainsKey(key))
+    {
+        dict.Add(key, new List<string>());
+    }
+
+    dict[key].Add(value);
+}
+
+foreach (var kvp in dict)
+{
+    Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+}
