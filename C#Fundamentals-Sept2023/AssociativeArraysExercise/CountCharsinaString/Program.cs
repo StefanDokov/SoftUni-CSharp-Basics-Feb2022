116f823f32338ea52155100b20383d8dd9812b25
StefanDokov <stefandokov2211@gmail.com>
2024-01-13 18:49:30 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/AssociativeArraysExercise/CountCharsinaString/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+
+
+
+
+using System.Text;
+
+List<string> first = Console.ReadLine()
+.Split()
+.ToList();
+
+if (first.Count > 1)
+
+{
+
+    Merge(first);
+
+}
+
+char[] word = first[0].ToCharArray();
+
+Dictionary<char, int> numberChar = new Dictionary<char, int>();
+
+foreach (var kvp in word)
+
+{
+
+    if (!numberChar.ContainsKey(kvp))
+
+    {
+
+        numberChar[kvp] = 0;
+
+    }
+
+
+
+    numberChar[kvp]++;
+
+}
+
+foreach (var kvp in numberChar)
+
+{
+
+    Console.WriteLine("{0} -> {1}", kvp.Key, kvp.Value);
+
+}
+
+
+
+static void Merge(List<string> first)
+
+{
+
+    StringBuilder currentWord = new StringBuilder();
+
+    for (int i = 0; i < first.Count; i++)
+
+    {
+
+        currentWord.Append(first[i]);
+
+    }
+
+    first.RemoveRange(0, first.Count - 1);
+
+    first.Insert(0, currentWord.ToString());
+
+}
