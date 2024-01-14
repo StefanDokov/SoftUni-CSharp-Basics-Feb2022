2a8bbba8d8ff579a9ff4f12a3d8915c4e3e8a000
StefanDokov <stefandokov2211@gmail.com>
2024-01-14 14:53:51 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/AssociativeArraysExercise/Courses/Program.cs
@@ -0,0 +1,42 @@
+﻿Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+Dictionary<string, int> counts = new Dictionary<string, int>();
+while (true)
+{
+    string[] input = Console.ReadLine().Split(" : ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    var course = input[0].Trim();
+
+    if (course == "end")
+    {
+        break;
+    }
+    var name = input[1];
+
+    if (!courses.ContainsKey(course))
+    {
+        courses.Add(course, new List<string>());
+
+    }
+
+    courses[course].Add(name);
+
+
+
+}
+PrintCoursesInfo(courses);
+            
+        
+
+        static void PrintCoursesInfo(Dictionary<string, List<string>> courses)
+{
+    foreach (var course in courses)
+    {
+        string courseName = course.Key;
+        var students = course.Value;
+        Console.WriteLine($"{courseName}: {students.Count}");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"-- {student}");
+        }
+
+    }
+}
---
+++ C#Fundamentals-Sept2023/AssociativeArraysExercise/SoftUniParking/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, string> parkingDatabase = new Dictionary<string, string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] command = Console.ReadLine().Split();
+
+    if (command[0] == "register")
+    {
+        string username = command[1];
+        string licensePlate = command[2];
+
+        if (parkingDatabase.ContainsKey(username))
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {parkingDatabase[username]}");
+        }
+        else
+        {
+            parkingDatabase[username] = licensePlate;
+            Console.WriteLine($"{username} registered {licensePlate} successfully");
+        }
+    }
+    else if (command[0] == "unregister")
+    {
+        string username = command[1];
+
+        if (parkingDatabase.ContainsKey(username))
+        {
+            Console.WriteLine($"{username} unregistered successfully");
+            parkingDatabase.Remove(username);
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {username} not found");
+        }
+    }
+}
+
+foreach (var entry in parkingDatabase)
+{
+    Console.WriteLine($"{entry.Key} => {entry.Value}");
+}
