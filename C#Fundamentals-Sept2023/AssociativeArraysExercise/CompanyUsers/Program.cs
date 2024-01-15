755b8472d2671285cb8751fd82a8314c1d29bddb
StefanDokov <stefandokov2211@gmail.com>
2024-01-15 19:17:54 +0200

    add more files

---
+++ C#Fundamentals-Sept2023/AssociativeArraysExercise/CompanyUsers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+Dictionary<string, List<string>> companyData = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] inputElements = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string iCompany = inputElements[0];
+    string iEmployeeID = inputElements[1];
+
+    if (!companyData.ContainsKey(iCompany))
+    {
+        companyData.Add(iCompany, new List<string>());
+    }
+
+    if (!companyData[iCompany].Contains(iEmployeeID))
+    {
+        companyData[iCompany].Add(iEmployeeID);
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var company in companyData)
+{
+    Console.WriteLine($"{company.Key}");
+    //foreach (var id in company.Value.OrderBy(id => id))
+    foreach (var id in company.Value)
+    {
+        Console.WriteLine($"-- {id}");
+    }
+}
---
+++ C#Fundamentals-Sept2023/AssociativeArraysExercise/StudentAcademy/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(name))
+    {
+        students[name] = new List<double>();
+    }
+
+    students[name].Add(grade);
+}
+
+var filteredStudents = students
+    .Where(s => s.Value.Average() >= 4.50)
+    .ToDictionary(s => s.Key, s => s.Value.Average());
+
+foreach (var student in filteredStudents)
+{
+    Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+}
