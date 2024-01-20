c9885327c83d487ca03e561b8e5fb1ff57475b59
StefanDokov <stefandokov2211@gmail.com>
2024-01-20 19:18:06 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/TextProcessing/RepeatStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+using System.Text;
+
+string input = Console.ReadLine();
+
+string[] stringsArray = input.Split(' ');
+
+foreach (string str in stringsArray)
+{
+    string repeatedString = RepeatString(str);
+    Console.Write(repeatedString);
+}
+    
+
+    static string RepeatString(string str)
+{
+    int length = str.Length;
+    StringBuilder repeatedStringBuilder = new StringBuilder(length * length);
+
+    for (int i = 0; i < length; i++)
+    {
+        repeatedStringBuilder.Append(str);
+    }
+
+    return repeatedStringBuilder.ToString();
+}
---
+++ C#Fundamentals-Sept2023/TextProcessing/ReverseStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "end")
+    {
+        break;
+    }
+
+    string reversed = ReverseString(input);
+    Console.WriteLine($"{input} = {reversed}");
+}
+    
+
+     string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
