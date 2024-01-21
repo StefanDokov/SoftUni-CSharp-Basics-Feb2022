a3bc055e16f69c251c7e5e608253066c83a53ec0
StefanDokov <stefandokov2211@gmail.com>
2024-01-21 08:51:34 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/TextProcessing/Substring/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+string key = Console.ReadLine();
+
+string text = Console.ReadLine();
+
+while (text.Contains(key))
+{
+    int index = text.IndexOf(key);
+
+    text = text.Remove(index, key.Length);
+}
+
+Console.WriteLine(text);
---
+++ C#Fundamentals-Sept2023/TextProcessing/TextFilter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+string[] banList = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+string text = Console.ReadLine();
+
+foreach (string banWord in banList)
+{
+    string replacement = new string('*', banWord.Length);
+    text = text.Replace(banWord, replacement);
+}
+
+Console.WriteLine(text);
