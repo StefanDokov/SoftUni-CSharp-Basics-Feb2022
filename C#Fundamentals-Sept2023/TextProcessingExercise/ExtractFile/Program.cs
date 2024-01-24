7b14c924e2985ef2d09c1fb6c8a76e6db5e8f731
StefanDokov <stefandokov2211@gmail.com>
2024-01-24 19:53:10 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/TextProcessingExercise/ExtractFile/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+string pathToAFile = Console.ReadLine();
+
+int startingIndexOtTheFileName = pathToAFile.LastIndexOf('\\');
+int endingIndexOtTheFileName = pathToAFile.IndexOf('.');
+
+string fileName = pathToAFile.Substring(startingIndexOtTheFileName + 1, endingIndexOtTheFileName -
+    startingIndexOtTheFileName - 1);
+string extension = pathToAFile.Substring(endingIndexOtTheFileName + 1);
+
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {extension}");
