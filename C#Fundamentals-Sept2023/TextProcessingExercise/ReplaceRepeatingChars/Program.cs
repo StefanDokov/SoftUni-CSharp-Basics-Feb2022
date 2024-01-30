01884394e6afe02a80aff8ab0b9548b6bdeba6f3
StefanDokov <stefandokov2211@gmail.com>
2024-01-30 18:08:17 +0200

    add more files
    add more files

---
+++ C#Fundamentals-Sept2023/TextProcessingExercise/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+
+string result = ReplaceConsecutiveDuplicates(input);
+
+Console.WriteLine(result);
+    
+
+    static string ReplaceConsecutiveDuplicates(string input)
+{
+    if (string.IsNullOrEmpty(input))
+    {
+        return input;
+    }
+
+    StringBuilder resultBuilder = new StringBuilder();
+
+    char currentChar = input[0];
+    resultBuilder.Append(currentChar);
+
+    for (int i = 1; i < input.Length; i++)
+    {
+        if (input[i] != currentChar)
+        {
+            resultBuilder.Append(input[i]);
+
+            currentChar = input[i];
+        }
+    }
+
+    return resultBuilder.ToString();
+}
