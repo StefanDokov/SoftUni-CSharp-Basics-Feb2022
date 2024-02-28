0983dfb276456486033141d2a013dbd9b6c68083
StefanDokov <stefandokov2211@gmail.com>
2024-02-28 21:49:21 +0200

    Regex Files
    Regex Files

---
+++ C#Fundamentals-Sept2023/RegularExpressions/MatchFullName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TechnologyFundamentalsPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+
+            MatchCollection names = Regex.Matches(input, pattern);
+
+            foreach (var name in names)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
